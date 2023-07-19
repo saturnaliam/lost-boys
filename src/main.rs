4862f2a1cf47efe9dc4236bf29d482f8b98002c3
Lucia Cover <llcsaturnaliam@gmail.com>
2023-07-18 20:13:45 -0500

    Added full parsing of the TOML template files.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,30 @@
 use std::fs;
 use std::io::ErrorKind;
+use serde::Deserialize;
+use std::env;
 
+#[derive(Deserialize)]
 struct CreatedFile {
     file_name: String,
     file_body: String,
 }
 
+#[derive(Deserialize)]
 struct Directory {
     directory_name: String,
-    subdirectories: Vec<Directory>,
-    files: Vec<CreatedFile>,
+    subfolders: Option<Vec<Directory>>,
+    files: Option<Vec<CreatedFile>>,
+}
+
+#[derive(Deserialize)]
+struct Project {
+    directories: Vec<Directory>,
+    files: Option<Vec<CreatedFile>>,
+}
+
+fn write_file(file_body: &String, file_name: &String) {
+    let new_file_body = format!("{file_body}\n");
+    fs::write(file_name, new_file_body).unwrap()
 }
 
 impl Directory {
@@ -27,37 +42,70 @@ impl Directory {
                 }
             },
         };
+        
 
-        for subfile in &mut self.files {
-            let CreatedFile { file_name, file_body } = subfile;
+        match &mut self.files {
+            Some(files) => {
+                for subfile in files {
+                    let CreatedFile { file_name, file_body } = subfile;
 
-            let new_file_name = format!("{self_directory_name}/{file_name}");
-            let new_file_body = format!("{file_body}\n");
-            fs::write(new_file_name, new_file_body).unwrap();
-        }
-    
-        for subdirectory in &mut self.subdirectories {
-            let directory_name = &mut subdirectory.directory_name;
-            let new_directory_name = format!("{self_directory_name}/{directory_name}");
-            subdirectory.directory_name = new_directory_name;
-            subdirectory.generate()
+                    let new_file_name = format!("{self_directory_name}/{file_name}");
+                    write_file(&file_body, &new_file_name);
+                }
+            },
+
+            None => {},
+        };
+   
+        match &mut self.subfolders {
+            Some(subfolders) => {
+                for subdirectory in subfolders {
+                    let directory_name = &mut subdirectory.directory_name;
+                    let new_directory_name = format!("{self_directory_name}/{directory_name}");
+                    subdirectory.directory_name = new_directory_name;
+                    subdirectory.generate()
+                }
+            },
+
+            None => {}, 
+        };
+    }
+}
+
+impl Project {
+    fn generate(&mut self) {
+        match &mut self.files {
+            Some(files) => {
+                for file in files {
+                    write_file(&file.file_body, &file.file_name)
+                }
+            }
+
+            None => {},
+        };
+
+        for directory in &mut self.directories {
+            directory.generate();
         }
     }
 }
 
-fn main() {
-    let mut dir = Directory {
-        directory_name: String::from("Test"),
-        subdirectories: Vec::new(),
-        files: Vec::new(),
+fn get_cli_path() -> String {
+    let exe_dir = env::current_exe().unwrap();
+    let exe_dir = exe_dir.to_str();
+
+    match exe_dir {
+        Some(dir) => return String::from(dir),
+        None => panic!("Error while reading the directory!"),
     };
+}
 
-    dir.subdirectories.push(Directory {
-        directory_name: String::from(":3"),
-        subdirectories: Vec::new(),
-        files:  vec![CreatedFile { file_name: String::from("test.txt"), file_body: String::from("deez") }],
-    });
+fn main() {
 
-    dir.files.push(CreatedFile { file_name: String::from("hello"), file_body: String::from("helloooo") });
-    dir.generate();
+
+    let contents = fs::read_to_string("templates/C.toml").unwrap();
+    let mut project: Project = toml::from_str(&contents).unwrap();
+    project.generate();
+    let exe_dir = get_cli_path();
+    println!("{exe_dir}");
 }
